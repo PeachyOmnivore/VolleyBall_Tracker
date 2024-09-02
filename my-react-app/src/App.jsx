b65d7684bc2e30816a0b1f8a3cbc8d6667f27cc2
Lukas <lad_777@hotmail.com>
2024-09-02 12:27:14 +0100

    feat: admin check and navigation to home if logged in

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,16 +1,44 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Login from "./pages/login/login";
 import Home from "./pages/home/home"
 import Registration from "./pages/registration/registration";
+import { useEffect, useState } from "react";
+import { get } from "./client-Functions";
 
 function App() {
+  const navigate = useNavigate()
+  const [currentUser, setCurrentUser] = useState('')
+  const [currentlyAdmin, setCurrentlyAdmin] = useState(false)
+
+  useEffect(() => {
+    async function findUser() {
+      try {
+        const foundUser = await get("users/me");
+
+        if (foundUser.foundUser.role === "ADMIN") {
+          setCurrentlyAdmin(true)
+        }
+        setCurrentUser(foundUser);
+
+        if (!foundUser) {
+          navigate('/')
+        }
+
+      } catch (err) {
+        console.error('Error fetching user info:', err);
+      }
+    }
+    findUser()
+  }, [navigate])
+
+
   return (
     <>
       <Routes>
         <Route path="login" element={<Login />} />
-        <Route path="home" element={<Home />} />
-        <Route path="registration" element={<Registration/>} />
+        <Route path="/" element={<Home user={currentUser}/>} />
+        <Route path="registration" element={<Registration />} />
       </Routes>
     </>
   );
